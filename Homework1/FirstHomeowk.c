a0da9b0cdd5803df645e080c37575f32f79e1eb3
Zeeshan Abidi <Zeeshanabidi24@hotmail.com>
2018-04-19 19:54:52 -0700

    Homework1

---
+++ Homework1/FirstHomeowk.c
@@ -0,0 +1,15 @@
+/*
+ * FirstHomeowk.c
+ *
+ *  Created on: Apr 19, 2018
+ *      Author: Zeeshan
+ */
+
+
+#include <stdio.h>
+
+int main(void)
+{
+  printf("Goodbye, I finished my CS 256 HW1\n");
+  return 0;
+}
